8bf932f812b7a29bdb9687470fbb20f3208f30f8
Ivan Paredes <i.a.paredes.rostek@gmail.com>
2023-11-29 15:13:09 -0300

    barra navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Sidebar from './Pages/Sidebar'
 import Homebar from './Pages/Homebar'
+import Navbar from './Pages/Navbar'
 import './App.css'
 
 function App() {
 
   return (
-    <div className='main'>
-      <Sidebar/>
-      <Homebar/>
-    </div>
+    <>
+      <Navbar/>
+        <div className='main'>
+          <Sidebar/>
+          <Homebar/>
+        </div>
+    </>
   )
 }
 
---
+++ src/Pages/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <input className='search' type='text' placeholder='Search'></input>
+    </div>
+  )
+}
+
+export default Navbar
